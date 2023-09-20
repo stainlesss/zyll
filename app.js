b1c28fcc4f7f0641635250e9545df6c6a0cfa4ce
stainless <54761166+stainlesss@users.noreply.github.com>
2023-09-20 13:35:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const fs = require('fs')
 
-var indexRouter = require('./routes/index');
+
 // var usersRouter = require('./routes/users');
 
 var app = express();
