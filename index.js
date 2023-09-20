fa29f73a9555ad76c5d7388ee0600c985434b580
stainless <54761166+stainlesss@users.noreply.github.com>
2023-09-20 13:35:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.get('/api/serials/', (req,res) => {
+	
+	try {
+  const data = fs.readFileSync('response.txt', 'utf8');
+  res.send(data)
+} catch (err) {
+   res.send(err)
+}
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
